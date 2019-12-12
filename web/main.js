1bbfe428f742c5dd62e282abf8b6eabf49b0826b
Blargian <shaunstruwig.ss@gmail.com>
2019-12-12 13:22:39 +0200

    Added comments to main.js Problem Solution Note

--- web/main.js
+++ web/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 function getWord(){
+
     word = String(document.getElementById("form_word").value);
     word = word.replace(/^\s+/g, '');
     //put the returned values into html elements
@@ -27,11 +28,22 @@ function getWord(){
 
     eel.get_image(word)(function(ret) {
 
+        //Clear the previous image grid population first
+        var retrieved_images = getElementById("retrieved_images");
+        while(retrieved_images.firstChild){
+            retrieved_images.removeChild(retrievedImages.firstChild);
+        }
+
+        //Create the first row within the div which has id="retrieved_images" and give it an id of row 0.
+        // row_number functions as a variable to be incremented when three images have been added per row
+        // and used within the naming of rows so that subsequent groups of three images are added to a 
+        //new row. 
+
         var ele_row = document.createElement("div");
         ele_row.className = "row";
         row_number = 0;
         ele_row.id = "row" + String(row_number);
-        console.log(ele_row.id);
+
         document.getElementById("retrieved_images").appendChild(ele_row);
 
         for (i = 0;i < 10; i++) {
@@ -40,11 +52,13 @@ function getWord(){
                 var ele_div = document.createElement("div");
                 ele_div.className = "col-md-4";
                 ele_div.id = "image_column" + String(i)
-                console.log(ele_div.id);
+
                 var ele_image = document.createElement("img");
                 ele_image.src=ret[i];
+
                 ele_image.width=200;
                 ele_image.height=200;
+
                 document.getElementById("row" + String(row_number)).appendChild(ele_div);
                 document.getElementById("image_column" + String(i)).appendChild(ele_image);
             } else {
